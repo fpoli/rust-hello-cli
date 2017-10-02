5fcd1bf1e5460e13aad4829be37dcd81f20cbea2
Federico Poli <federpoli@gmail.com>
2017-10-02 13:28:00 +0200

    Move unit tests into src/

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,20 @@ pub fn build_message(opt_name: Option<&str>) -> String {
     let name = opt_name.unwrap_or("World");
     return format!("Hello {}!", name);
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn build_message_none() {
+        let message = build_message(None);
+        assert_eq!(message, "Hello World!");
+    }
+
+    #[test]
+    fn build_message_some() {
+        let message = build_message(Some("MyName"));
+        assert_eq!(message, "Hello MyName!");
+    }
+}
